7e934adacdc26d48f59b9619e2861d959c4c58ca
Rowshon Habib Rony <rhrony0009@gmail.com>
2022-03-06 00:32:36 +0600

    add db.php v1.1.0

--- assets/db.php
+++ assets/db.php
@@ -7,5 +7,5 @@ define('DB_NAME', 'cit');
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if (!$conn) {
-    die('Connection failed: ' . mysqli_connect_error());
+    die('Connection Failed: ' . mysqli_connect_error());
 }
